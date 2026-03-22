6ee3ae08b73f4a0f9ddd03b8f027a064237869f4
Ricky Concepcion <rcon@pm.me>
2026-03-22 00:13:05 -0400

    revert polling

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from nicegui import app, ui, core
+from nicegui import app, ui
 
 from gui.doll_calculator.dolls.robella import Robella
 from gui.doll_calculator.dolls.voymastina import Voymastina
@@ -6,10 +6,6 @@ from gui.doll_calculator.dolls.lewis import Lewis
 from gui.doll_calculator.dolls.mosin_nagant import MosinNagant
 from gui.doll_calculator.dolls.tololo import Tololo
 from gui.doll_calculator.dolls.leva import Leva
-
-core.app.config.socket_io_js_transports = [
-    "polling",
-]
 
 
 def root():
@@ -104,5 +100,5 @@ if __name__ in {"__main__", "__mp_main__"}:
     ui.run(
         root,
         reload=False,
-        reconnect_timeout=60,
+        reconnect_timeout=300,
     )
