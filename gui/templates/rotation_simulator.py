f8ffb2bce6d0eb6135322053a3bda63aa58242fb
Ricky Concepcion <rcon@pm.me>
2026-07-28 20:44:20 -0400

    Fix an issue with async callback wiring causing Rotation Simulation to not execute

--- gui/templates/rotation_simulator.py
+++ gui/templates/rotation_simulator.py
@@ -1818,7 +1818,7 @@ class RotationSimulator:
         )
 
     def _start_simulation(self) -> None:
-        self.simulate_button.client.safe_invoke(self._run_simulation())
+        self.simulate_button.client.safe_invoke(self._run_simulation)
 
     async def _run_simulation(self) -> None:
         try:
